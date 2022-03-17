0486d3402122789205739628742de91ed8850026
Elias Balmer <elias.balmer@zuehlke.com>
2022-03-17 12:32:04 +0100

    further small change

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express'); //require is a Node.js function to load modules
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hoi zämme!'));
+app.get('/', (req, res) => res.send('Hoi zämme! Leider hets hütt nid klappet.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
